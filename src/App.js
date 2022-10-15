ae88e07412dde45703ce938a5b2b0b412b66cc16
Myles Gaerlan <ish.myles@gmail.com>
2022-10-15 16:00:18 +1100

    Restructured project directory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';  --TODO: Create logo & favicon for app
+import './assets/styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Markdown Previewer</h1>
     </div>
   );
 }
